fae4ff3b3f4de9715b7a390a9f1b5e888e310f7c
MarcinSzymanek <48028985+MarcinSzymanek@users.noreply.github.com>
2022-05-25 20:58:24 +0200

    Added several screens (fragments), functionality on button presses (sends ws message and switches screen) and in general most of the implementation. Added some nice paw icons and coloured buttons to relevant screens. All icon assets from https://www.flaticon.com/free-icons/paw author : Creatype

---
+++ app/src/main/java/com/example/websocketprotoandroid/FragmentOnConnecting.kt
@@ -0,0 +1,9 @@
+package com.example.websocketprotoandroid
+
+import androidx.fragment.app.Fragment
+
+// This fragment is meant to show a connecting animation to the user as a transition
+// between not being connected with the server and being connected
+class FragmentOnConnecting : Fragment(){
+    // UNIMPLEMENTED
+}
